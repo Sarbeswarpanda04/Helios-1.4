af7def295a75a2bc26bc7c280f0aa818739f8857
Sarbeswar panda <sarbeswarpanda143@gmail.com>
2026-09-05 21:11:56 +0530

    class 9

---
+++ class 9/script.js
@@ -0,0 +1,27 @@
+// Function
+// alert("Warning! This is a test alert message.");
+
+// let n = prompt("Enter a number: ");
+// console.log("You entered: " + n, "Type of n: " + typeof n);
+
+//user defined function
+//normal function
+
+function add(a, b) {
+  return a + b;
+}
+console.log(add(5, 10));
+
+//arrow function
+const multiply = (a, b) => {
+  return a * b;
+};
+console.log(multiply(5, 10));
+
+//anonymous function
+const divide = function (a, b) {
+  return a / b;
+};
+console.log(divide(10, 5));
+
+// Array
